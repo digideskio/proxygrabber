40c49044ff32e5629dd302cacce96dd402756a5e
Mauro Marano <gagginaspinnata@MBP-di-Mauro.homenet.telecomitalia.it>
2016-04-07 16:57:29 +0200

    removed the output. Now it just print the output to stdout

--- bin/proxy_grabber.js
+++ bin/proxy_grabber.js
@@ -3,14 +3,6 @@
 var shell = require('shelljs');
 var program = require('commander');
 
-var output = "result.txt"
-
-program
- .option('-o, --output <output>', 'Where to save de result, default result.txt.')
- .parse(process.argv);
-
-
-if (program.output) output = program.output;
 
 
 shell.exec('\
@@ -20,17 +12,12 @@ shell.exec('\
 	sed "s/<tr><td>/ /g" | \
 	awk \'{print $1,$2}\' | \
 	 sed "s/ /:/g" | \
-	 grep -E "[0-9]{1,4}\.[0-9]{1,4}\.[0-9]{1,4}\.[0-9]{1,4}:[0-9]{1,5}" \
-	 | tee ' + output)
-.stdout;
+	 grep -E "[0-9]{1,4}\.[0-9]{1,4}\.[0-9]{1,4}\.[0-9]{1,4}:[0-9]{1,5}"').stdout;
 
 shell.exec('\
 curl -s --connect-timeout 5 --retry 3 "http://gatherproxy.com/proxylist/port/8080" | \
   grep -Eo "([0-9]{1,4}\.[0-9]{1,4}\.[0-9]{1,4}\.[0-9]{1,4})" | \
-  while read var; do echo $var:8080; done\
-  | tee ' + output,
-{"silent": true})
-.stdout;
+  while read var; do echo $var:8080; done').stdout;
 
 shell.exec('curl -s --connect-timeout 5 --retry 3 "http://proxyrox.com/?p=9" | \
   grep "/proxy/" | \
@@ -38,8 +25,7 @@ shell.exec('curl -s --connect-timeout 5 --retry 3 "http://proxyrox.com/?p=9" | \
   cut -f3 -d"/" | \
   sed "s/-/:/g" | \
   sed \'s/"//g\' | \
-  grep -E "[0-9]{1,4}\.[0-9]{1,4}\.[0-9]{1,4}\.[0-9]{1,4}:[0-9]{1,5}"\
-  | tee ' + output,{"silent":true}).stdout;
+  grep -E "[0-9]{1,4}\.[0-9]{1,4}\.[0-9]{1,4}\.[0-9]{1,4}:[0-9]{1,5}"').stdout;
 
 shell.exec('curl -s --connect-timeout 5 --retry 3 \
   "https://proxy-list.org/english/search.php?search=ssl-no&country=any&type=any&port=any&ssl=no" | \
@@ -48,8 +34,7 @@ shell.exec('curl -s --connect-timeout 5 --retry 3 \
   while read var; do\
     echo $var | base64 --decode ;\
     echo " ";\
-  done\
-  | tee ' + output,{"silent":true}).stdout;
+  done').stdout;
 
 shell.exec('curl -s --connect-timeout 5 --retry 3 "https://free-proxy-list.net/" | \
   grep -E "[0-9]{1,4}\.[0-9]{1,4}\.[0-9]{1,4}\.[0-9]{1,4}" | \
@@ -57,5 +42,4 @@ shell.exec('curl -s --connect-timeout 5 --retry 3 "https://free-proxy-list.net/"
   sed "s/<tr><td>/ /g" | \
   awk \'{print $1,$2}\' | \
   sed "s/ /:/g" | \
-  grep -E "[0-9]{1,4}\.[0-9]{1,4}\.[0-9]{1,4}\.[0-9]{1,4}:[0-9]{1,5}"\
-  | tee ' + output,{"silent":true}).stdout;
+  grep -E "[0-9]{1,4}\.[0-9]{1,4}\.[0-9]{1,4}\.[0-9]{1,4}:[0-9]{1,5}"').stdout;
